2ceb85fc7440bda9825dba52c3f76df915811259
Aleksei Cherniaev <fixgn@outlook.com>
2022-04-02 17:24:24 +0400

    Static method FromRental was added to RentalViewModel

--- VacationRental.Api/ViewModels/RentalViewModel.cs
+++ VacationRental.Api/ViewModels/RentalViewModel.cs
@@ -1,8 +1,19 @@
-﻿namespace VacationRental.Api.ViewModels
+﻿using VacationRental.Api.Models;
+
+namespace VacationRental.Api.ViewModels
 {
     public class RentalViewModel
     {
         public int Id { get; set; }
         public int Units { get; set; }
+
+        public static RentalViewModel FromRental(Rental rental)
+        {
+            return new RentalViewModel
+            {
+                Id = rental.Id,
+                Units = rental.Units
+            };
+        }
     }
 }
