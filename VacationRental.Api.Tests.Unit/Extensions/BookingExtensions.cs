118fda9e7820a3bda3288ae786b90a65465da5d5
Aleksei Cherniaev <fixgn@outlook.com>
2022-04-03 19:57:52 +0400

    Tests.Unit: Methods AreEqual in Booking and GetCalendarDatesResult extensions were minified;

--- VacationRental.Api.Tests.Unit/Extensions/BookingExtensions.cs
+++ VacationRental.Api.Tests.Unit/Extensions/BookingExtensions.cs
@@ -6,14 +6,9 @@ internal static class BookingExtensions
 {
     public static bool AreEqual(this Booking booking1, Booking booking2)
     {
-        if (booking1.Id == booking2.Id
-            && booking1.RentalId == booking2.RentalId
-            && booking1.Start == booking2.Start
-            && booking1.Nights == booking2.Nights)
-        {
-            return true;
-        }
-
-        return false;
+        return booking1.Id == booking2.Id
+               && booking1.RentalId == booking2.RentalId
+               && booking1.Start == booking2.Start
+               && booking1.Nights == booking2.Nights;
     }
 }
--- VacationRental.Api.Tests.Unit/Extensions/GetCalendarDatesResultExtension.cs
+++ VacationRental.Api.Tests.Unit/Extensions/GetCalendarDatesResultExtension.cs
@@ -6,13 +6,8 @@ public static class GetCalendarDatesResultExtension
 {
     public static bool AreEqual(this GetCalendarDatesResult result1, GetCalendarDatesResult result2)
     {
-        if (result1.IsSuccess == result2.IsSuccess
-            && result1.ErrorMessage == result2.ErrorMessage
-            && result1.CalendarDates.AreEqual(result2.CalendarDates))
-        {
-            return true;
-        }
-
-        return false;
+        return result1.IsSuccess == result2.IsSuccess
+               && result1.ErrorMessage == result2.ErrorMessage
+               && result1.CalendarDates.AreEqual(result2.CalendarDates);
     }
 }
