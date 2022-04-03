c530b7cf748b6caac1715e5d4142f59bd064272d
Aleksei Cherniaev <fixgn@outlook.com>
2022-04-03 16:12:01 +0400

    Tests.Unit: Extensions for CalendarDates and GetCalendarDatesResult were created

---
+++ VacationRental.Api.Tests.Unit/Extensions/GetCalendarDatesResultExtension.cs
@@ -0,0 +1,18 @@
+using VacationRental.Api.Services.Models;
+
+namespace VacationRental.Api.Tests.Unit.Extensions;
+
+public static class GetCalendarDatesResultExtension
+{
+    public static bool AreEqual(this GetCalendarDatesResult result1, GetCalendarDatesResult result2)
+    {
+        if (result1.IsSuccess == result2.IsSuccess
+            && result1.ErrorMessage == result2.ErrorMessage
+            && result1.CalendarDates.AreEqual(result2.CalendarDates))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
