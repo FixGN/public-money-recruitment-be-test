bb2ae91c941f15e46dfba1ffc7ce5d834ef1c6a3
Aleksei Cherniaev <fixgn@outlook.com>
2022-04-04 16:08:48 +0400

    Restrictions were added to BindingModel in contracts

--- VacationRental.Api.Contracts/Booking/BookingBindingModel.cs
+++ VacationRental.Api.Contracts/Booking/BookingBindingModel.cs
@@ -1,16 +1,32 @@
-﻿namespace VacationRental.Api.Contracts.Booking
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VacationRental.Api.Contracts.Booking
 {
     public class BookingBindingModel
     {
         public BookingBindingModel(int rentalId, DateTime start, int nights)
         {
+            if (rentalId < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rentalId),"Rental id cannot be negative");
+            }
+
+            if (nights < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(nights),"Nights cannot be negative");
+            }
+
             RentalId = rentalId;
             Start = start.Date;
             Nights = nights;
         }
         
+        [Range(0, int.MaxValue)]
         public int RentalId { get; }
+        [Required]
         public DateTime Start { get; }
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Nights { get; }
     }
 }
--- VacationRental.Api.Contracts/Rental/RentalBindingModel.cs
+++ VacationRental.Api.Contracts/Rental/RentalBindingModel.cs
@@ -1,12 +1,20 @@
-﻿namespace VacationRental.Api.Contracts.Rental
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VacationRental.Api.Contracts.Rental
 {
     public class RentalBindingModel
     {
         public RentalBindingModel(int units)
         {
+            if (units < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(units), "Units must be greater than 0");
+            }
+
             Units = units;
         }
         
+        [Range(0, int.MaxValue)]
         public int Units { get; }
     }
 }
