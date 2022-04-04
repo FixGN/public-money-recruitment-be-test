31a6c85184f7b0f1deb12564097754b7efd91479
Aleksei Cherniaev <fixgn@outlook.com>
2022-04-04 16:25:37 +0400

    Parameterless constructors were added to BindingModels

--- VacationRental.Api.Contracts/Booking/BookingBindingModel.cs
+++ VacationRental.Api.Contracts/Booking/BookingBindingModel.cs
@@ -1,7 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace VacationRental.Api.Contracts.Booking
 {
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class BookingBindingModel
     {
         public BookingBindingModel(int rentalId, DateTime start, int nights)
@@ -21,12 +25,18 @@ namespace VacationRental.Api.Contracts.Booking
             Nights = nights;
         }
         
+        /// <summary>Parameterless constructor for System.Text.Json.</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public BookingBindingModel()
+        {
+        }
+        
         [Range(0, int.MaxValue)]
-        public int RentalId { get; }
+        public int RentalId { get; set; }
         [Required]
-        public DateTime Start { get; }
+        public DateTime Start { get; set; }
         [Required]
         [Range(1, int.MaxValue)]
-        public int Nights { get; }
+        public int Nights { get; set; }
     }
 }
--- VacationRental.Api.Contracts/Rental/RentalBindingModel.cs
+++ VacationRental.Api.Contracts/Rental/RentalBindingModel.cs
@@ -1,7 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace VacationRental.Api.Contracts.Rental
 {
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class RentalBindingModel
     {
         public RentalBindingModel(int units)
@@ -13,8 +17,14 @@ namespace VacationRental.Api.Contracts.Rental
 
             Units = units;
         }
+
+        /// <summary>Parameterless constructor for System.Text.Json.</summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public RentalBindingModel()
+        {
+        }
         
         [Range(0, int.MaxValue)]
-        public int Units { get; }
+        public int Units { get; set; }
     }
 }
