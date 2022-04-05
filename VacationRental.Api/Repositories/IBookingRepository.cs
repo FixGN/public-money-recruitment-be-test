a838e55bdbccb23db62b125d7ced8b6e57ea59f1
Aleksei Cherniaev <fixgn@outlook.com>
2022-04-05 23:11:32 +0400

    Method GetByRentalId was added to IBookingRepository and DictionaryBookingRepository

--- VacationRental.Api/Repositories/IBookingRepository.cs
+++ VacationRental.Api/Repositories/IBookingRepository.cs
@@ -6,6 +6,7 @@ namespace VacationRental.Api.Repositories;
 public interface IBookingRepository
 {
     public Booking? GetOrDefault(int id);
+    public Booking[] GetByRentalId(int rentalId);
     public Booking[] GetByRentalIdAndDatePeriod(int rentalId, DateTime startDate, DateTime endDate);
     public Booking Create(int rentalId, int unit, DateTime start, int nights);
 }
