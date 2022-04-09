90b250c2d66fb9ecd0cc58e5818c25f96343a3eb
Aleksei Cherniaev <fixgn@outlook.com>
2022-04-09 13:51:21 +0400

    Tests.Unit: CalendarDatesArrayExtension.AreEqual was actualized

--- VacationRental.Api.Tests.Unit/Extensions/CalendarDatesArrayExtension.cs
+++ VacationRental.Api.Tests.Unit/Extensions/CalendarDatesArrayExtension.cs
@@ -12,8 +12,38 @@ public static class CalendarDatesArrayExtension
             return false;
         }
 
-        return !calendarDates1
-            .Where((calendarDate, i) => calendarDate.Date != calendarDates2[i].Date)
-            .Any();
+        foreach (var calendarDate1 in calendarDates1)
+        {
+            var calendarDate2 = calendarDates2.FirstOrDefault(x => x.Date == calendarDate1.Date);
+
+            if (calendarDate2 == null)
+            {
+                return false;
+            }
+
+            if (calendarDate1.Bookings.Length != calendarDate2.Bookings.Length
+                && calendarDate1.PreparationTimes.Length != calendarDate2.PreparationTimes.Length)
+            {
+                return false;
+            }
+            
+            var bookingsAreEqual = calendarDate1.Bookings
+                .All(x => calendarDate2.Bookings.Any(x.AreEqual));
+
+            if (!bookingsAreEqual)
+            {
+                return false;
+            }
+
+            var preparationTimesAreEqual = calendarDate1.PreparationTimes
+                .All(pt1 => calendarDate2.PreparationTimes.Any(pt2 => pt1.Unit == pt2.Unit));
+
+            if (!preparationTimesAreEqual)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
