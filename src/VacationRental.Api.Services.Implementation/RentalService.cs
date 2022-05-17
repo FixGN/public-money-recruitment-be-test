19122c3f7a93afe5efdd31acf2fcfc603f150975
Aleksei Cherniaev <fixgn@outlook.com>
2022-05-17 11:02:08 +0400

    RentalService.UpdateRentalAsync was refactored in conflict checks

--- src/VacationRental.Api.Services.Implementation/RentalService.cs
+++ src/VacationRental.Api.Services.Implementation/RentalService.cs
@@ -84,53 +84,56 @@ public class RentalService : IRentalService
             return UpdateRentalResult.Successful(rental);
         }
 
-        var rentalBookings = await _bookingRepository.GetByRentalIdAsync(id, cancellationToken);
-        if (rentalBookings.Length != 0)
+        // if (rental.Units <= units && rental.PreparationTimeInDays >= preparationTimeInDays) - can update rental without checks
+        if (rental.Units > units || rental.PreparationTimeInDays < preparationTimeInDays)
         {
-            var minRentalDate = rentalBookings.Select(x => x.Start).Min();
-            var maxRentalDate = rentalBookings.Select(x => x.Start.AddDays(x.Nights + rental.PreparationTimeInDays - 1)).Max();
-            var currentBookingDaysCount = maxRentalDate.DayNumber - minRentalDate.DayNumber + 1;
-
-            for (var i = 0; i < currentBookingDaysCount; i++)
+            var rentalBookings = await _bookingRepository.GetByRentalIdAsync(id, cancellationToken);
+            if (rentalBookings.Length != 0)
             {
-                var date = minRentalDate.AddDays(i);
-                var bookingsWithNewPreparationTime = rentalBookings
-                    .Where(x => x.Start <= date
-                                && date <= x.Start.AddDays(x.Nights + preparationTimeInDays - 1))
-                    .ToList();
+                var minRentalDate = rentalBookings.Select(x => x.Start).Min();
+                var maxRentalDate = rentalBookings.Select(x => x.Start.AddDays(x.Nights + rental.PreparationTimeInDays - 1)).Max();
+                var currentBookingDaysCount = maxRentalDate.DayNumber - minRentalDate.DayNumber + 1;
 
-                if (bookingsWithNewPreparationTime.Count != bookingsWithNewPreparationTime.Select(x => x.Unit).Distinct().Count())
+                for (var i = 0; i < currentBookingDaysCount; i++)
                 {
-                    _logger.UpdateRentalAsyncNumberOfUnitsConflict(id, units, preparationTimeInDays, date);
-                    return UpdateRentalResult.Conflict(
-                        $"Preparation time in days '{preparationTimeInDays}' makes conflict with bookings on date '{date:d}'");
-                }
+                    var date = minRentalDate.AddDays(i);
+                    var bookingsWithNewPreparationTime = rentalBookings
+                        .Where(x => x.Start <= date
+                                    && date <= x.Start.AddDays(x.Nights + preparationTimeInDays - 1))
+                        .ToList();
 
-                if (units < bookingsWithNewPreparationTime.Count)
-                {
-                    _logger.UpdateRentalAsyncNumberOfPreparationTimeInDaysConflict(
-                        id,
-                        units,
-                        preparationTimeInDays,
-                        date,
-                        bookingsWithNewPreparationTime.Count);
-                    return UpdateRentalResult.Conflict(
-                        $"Units count too small for current bookings (bookings count for day {date:d} is {bookingsWithNewPreparationTime.Count})");
+                    if (bookingsWithNewPreparationTime.Count != bookingsWithNewPreparationTime.Select(x => x.Unit).Distinct().Count())
+                    {
+                        _logger.UpdateRentalAsyncNumberOfUnitsConflict(id, units, preparationTimeInDays, date);
+                        return UpdateRentalResult.Conflict(
+                            $"Preparation time in days '{preparationTimeInDays}' makes conflict with bookings on date '{date:d}'");
+                    }
+
+                    if (units < bookingsWithNewPreparationTime.Count)
+                    {
+                        _logger.UpdateRentalAsyncNumberOfPreparationTimeInDaysConflict(
+                            id,
+                            units,
+                            preparationTimeInDays,
+                            date,
+                            bookingsWithNewPreparationTime.Count);
+                        return UpdateRentalResult.Conflict(
+                            $"Units count too small for current bookings (bookings count for day {date:d} is {bookingsWithNewPreparationTime.Count})");
+                    }
                 }
             }
         }
 
-        var updatedRental = new Rental(rental.Id, units, preparationTimeInDays, rental.Version + 1);
+        var newRental = new Rental(rental.Id, units, preparationTimeInDays, rental.Version + 1);
         try
         {
-            await _rentalRepository.UpdateAsync(updatedRental, cancellationToken);
+            await _rentalRepository.UpdateAsync(newRental, cancellationToken);
         }
         catch (DBConcurrencyException e)
         {
-            _logger.UpdateRentalAsyncDbConcurrencyExceptionWasThrown(id, units, preparationTimeInDays, e.Message);
+            _logger.UpdateRentalAsyncDbConcurrencyExceptionWasThrown(rental.Id, units, preparationTimeInDays, e.Message);
             return UpdateRentalResult.ConcurrencyException(e.Message);
         }
-        _logger.UpdateRentalAsyncEnd(id, units, preparationTimeInDays);
-        return UpdateRentalResult.Successful(updatedRental);
+        return UpdateRentalResult.Successful(newRental);
     }
 }
